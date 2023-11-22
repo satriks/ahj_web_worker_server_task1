8253943ff2f0b875623167e79c5dc362c61e16df
Yury Vereskun <satriks@mail.ru>
2023-11-22 03:29:52 +0300

    upd resp for /

--- routers/news/index.js
+++ routers/news/index.js
@@ -3,30 +3,6 @@ const db = require('../../db/db')
 const router = new Router()
 
 router.get('/news', async (ctx) => {
-  // const broken = Math.random()
-  // if (broken < 0.5) {
-  //   ctx.response.status = 500
-  //   return
-  // }
-
-  // ctx.set('Access-Control-Allow-Origin', '*'); // Allow requests from all domain names (the request does not carry the cookie can be used * if there carrying cookie request must specify the domain name)
-  // // // ctx.set ( "Access-Control-Allow-Origin", "http: // localhost: 8080"); // Allow only the domain name http: // localhost: 8080 request
-
-  // ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE'); // settings allow HTTP request method
-
-  // ctx.set('Access-Control-Allow-Headers', 'x-requested-with, accept, origin, content-type'); // field is required. It is also a comma-delimited string, indicating all the header fields supported by the server.
-  // // // // After the server receives the request, check the Origin, after the Access-Control-Request-Method and Access-Control-Request-Headers field to verify that allow cross-origin requests, we can respond.
-
-  // ctx.set('Content-Type', 'application/json;charset=utf-8'); // Content-Type represents a particular media type information request
-
-  // // ctx.set('Access-Control-Allow-Credentials', true)
-
-  // ctx.set('Service-Worker-Allow' , '/' );
-
-  // ctx.responseHeaders.set('Access-Control-Allow-Headers', "*")
-  // ctx.responseHeaders.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
-  // ctx.responseHeaders.set('Access-Control-Allow-Headers', 'x-requested-with, accept, origin, content-type')
-  // ctx.responseHeaders.set('Access-Control-Allow-Credentials', true)
 
   console.log('ping')
   ctx.response.body = {
--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const Koa = require('koa')
 const { koaBody } = require('koa-body')
 const { Date } = require('core-js')
 const cors = require('koa2-cors')
-const port = process.env.port  || 7070
+const port = process.env.port || 7070
 const app = new Koa()
 const slow = require('koa-slow')
 const createNews = require('./generator/generatorControl')
 app.use(cors())
-
-
 
 const router = require('./routers')
 
@@ -18,22 +16,11 @@ app.use(koaBody({
   multipart: true
 }))
 
-// app.use(async (ctx, next) => {
-//   ctx.set('Access-Control-Allow-Origin', 'https://satriks.github.io'); // Allow requests from all domain names (the request does not carry the cookie can be used * if there carrying cookie request must specify the domain name)
-//   // // ctx.set ( "Access-Control-Allow-Origin", "http: // localhost: 8080"); // Allow only the domain name http: // localhost: 8080 request
+app.use((ctx, next) => {
+  ctx.body = " Сервер работает,  тест"
+  next()
+})
 
-//   ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE'); // settings allow HTTP request method
-
-//   ctx.set('Access-Control-Allow-Headers', 'x-requested-with, accept, origin, content-type'); // field is required. It is also a comma-delimited string, indicating all the header fields supported by the server.
-//   // // // After the server receives the request, check the Origin, after the Access-Control-Request-Method and Access-Control-Request-Headers field to verify that allow cross-origin requests, we can respond.
-
-//   ctx.set('Content-Type', 'application/json;charset=utf-8'); // Content-Type represents a particular media type information request
-
-//   ctx.set('Access-Control-Allow-Credentials', true)
-
-//   ctx.set('Service-Worker-Allow' , '/' );
-//   await next();
-// });
 
 app.use(slow({ delay: 5000 }))
 app.use(router())
